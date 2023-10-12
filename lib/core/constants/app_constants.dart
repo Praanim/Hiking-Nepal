6b6a61e5715afb6e5144a302a8b0d283e00b7660
Praanim <pranimsingh7@gmail.com>
2023-10-12 09:33:27 +0545

    little work

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,4 +2,19 @@ class AppConstants {
   //border radius
   static const double primaryBorderRadius = 10;
   static const double secondaryBorderRadius = 20;
+
+  ///provides padding of 2 pixel
+  static const double pad2 = 2;
+
+  ///provides padding of 4 pixel
+  static const double pad4 = 4;
+
+  ///provides padding of 6 pixel
+  static const double pad6 = 6;
+
+  ///provides padding of 8 pixel
+  static const double pad8 = 8;
+
+  ///provides padding of 8 pixel
+  static const double pad10 = 10;
 }
--- lib/features/post/presentation/pages/add_post_page.dart
+++ lib/features/post/presentation/pages/add_post_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hiking_nepal/core/constants/app_constants.dart';
 import 'package:hiking_nepal/core/constants/app_media.dart';
 import 'package:hiking_nepal/core/extensions/helper_extension.dart';
 import 'package:hiking_nepal/core/theme/app_colors.dart';
+import 'package:hiking_nepal/core/utils/gap.dart';
 
 class AddPostPage extends StatefulWidget {
   const AddPostPage({super.key});
@@ -11,6 +13,17 @@ class AddPostPage extends StatefulWidget {
 }
 
 class _AddPostPageState extends State<AddPostPage> {
+  late TextEditingController _nameController;
+  late TextEditingController _descriptionController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _nameController = TextEditingController();
+    _descriptionController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,17 +36,62 @@ class _AddPostPageState extends State<AddPostPage> {
       body: PageView(
         scrollDirection: Axis.horizontal,
         children: [
+          ///image section
+
           Column(
             children: [
               SizedBox(
-                height: context.height * 0.4,
+                height: context.height * 0.3,
+                width: context.width,
                 child: Stack(
                   children: [
                     Container(
+                      height: context.height * 0.25,
                       decoration: BoxDecoration(
                           color: Theme.of(context).scaffoldBackgroundColor,
                           image: const DecorationImage(
-                              image: AssetImage(AppMedia.addPostCoverImage))),
+                              image: AssetImage(AppMedia.addPostCoverImage),
+                              fit: BoxFit.cover)),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: context.width * 0.39,
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundColor:
+                            Theme.of(context).colorScheme.secondary,
+                        child: CircleAvatar(
+                          backgroundColor: LightColor.secondaryBackgroundColor,
+                          radius: 40,
+                          child: IconButton(
+                            onPressed: () {
+                              ///add image
+                            },
+                            icon: Icon(
+                              Icons.camera_alt,
+                              size: 40,
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              VerticalGap.l,
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: AppConstants.pad8),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration:
+                          _makePageInputDecoration("Name of Destination"),
+                    ),
+                    VerticalGap.s,
+                    TextFormField(
+                      decoration: InputDecoration(),
                     )
                   ],
                 ),
@@ -49,5 +107,13 @@ class _AddPostPageState extends State<AddPostPage> {
         ],
       ),
     );
+  }
+
+  InputDecoration _makePageInputDecoration(String labelText) {
+    return InputDecoration(
+        labelText: labelText,
+        labelStyle: context.textTheme.bodySmall,
+        filled: true,
+        fillColor: LightColor.secondaryBackgroundColor);
   }
 }
